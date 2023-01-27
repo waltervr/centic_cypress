ca135d44b91af7430393524b8855abe423ddb717
Walter Vargas <walter.vargas@avantica.net>
2023-01-26 21:21:35 -0600

    paso 3

--- cypress/e2e/todomvc.cy.js
+++ cypress/e2e/todomvc.cy.js
@@ -1,3 +1,6 @@
-it('should navigate to the TodoMVC App', () => {
+it('should add a new todo to the list', () => {
   cy.visit('http://todomvc-app-for-testing.surge.sh/')
+  cy.get('.new-todo').type('Clean room{enter}')
+  cy.get('.toggle').click()
+  cy.contains('Clear completed').click()
 })
