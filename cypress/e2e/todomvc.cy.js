8a20a7de12212b57314e19a979452e03ea0265fb
Walter Vargas <walter.vargas@avantica.net>
2023-02-01 18:12:57 -0600

    paso-5

--- cypress/e2e/todomvc.cy.js
+++ cypress/e2e/todomvc.cy.js
@@ -1,10 +1,25 @@
-it('should add a new todo to the list', () => {
-  cy.visit('http://todomvc-app-for-testing.surge.sh/')
-  cy.get('.new-todo').type('Clean room{enter}')
-  cy.get('label').should('have.text', 'Clean room')
-  cy.get('.toggle').should('not.be.checked')
-  cy.get('.toggle').click()
-  cy.get('label').should('have.css', 'text-decoration-line', 'line-through')
-  cy.contains('Clear completed').click()
-  cy.get('.todo-list').should('not.have.descendants', 'li')
+describe('filtering', function() {
+
+  beforeEach(() => {
+    cy.visit('http://todomvc-app-for-testing.surge.sh/')
+    cy.get('.new-todo').type('Clean room{enter}')
+    cy.get('.new-todo').type('Learn JavaScript{enter}')
+    cy.get('.new-todo').type('Use Cypress{enter}')
+    cy.get('.todo-list li:nth-child(2) .toggle').click()
+  })
+
+  it('should filter "Active" correctly', () => {
+    cy.contains('Active').click()
+    cy.get('.todo-list li').should('have.length', 2)
+  })
+
+  it('should filter "Completed" correctly', () => {
+    cy.contains('Completed').click()
+    cy.get('.todo-list li').should('have.length', 1)
+  })
+
+  it('should filter "All" correctly', () => {
+    cy.contains('All').click()
+    cy.get('.todo-list li').should('have.length', 3)
+  })
 })
